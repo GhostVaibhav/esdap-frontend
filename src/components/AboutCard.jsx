0a3422b345a42c18073ffb3f8dc0ecea062240b1
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-04-30 23:05:51 +0530

    add(home-card, login-page): added animation to the pages

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function AboutCard() {
   return (
-    <div>AboutCard</div>
+    <div className='text-white p-16 px-36 h-full flex-grow flex flex-col justify-center animate__animated animate__fadeIn'>
+      <div className='pb-8'>
+        <div className='text-5xl'>About</div>
+      </div>
+      <div className='font-extralight' style={{ fontFamily: "Arial" }}>
+        The scope of the project encompasses a comprehensive examination of the operational
+        dynamics within educational institutions, particularly focusing on the intricacies of data
+        flow and encryption logic. This includes an in-depth exploration of the workings of
+        schools, as well as collaboration with a third-party analytics company to gain insights
+        into data manipulation practices. The project aims to elucidate the entire data flow
+        process, from data collection to analysis, while emphasizing the importance of
+        encryption in safeguarding sensitive information. Additionally, the project will involve
+        the development of a frontend interface to provide a user-friendly portrayal of the data
+        analytics process, enhancing accessibility and understanding for stakeholders involved.
+      </div>
+    </div>
   )
 }
 
--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeCard() {
   return (
-    <div className='text-white p-16 px-36 h-full flex-grow flex flex-col justify-center'>
+    <div className='text-white p-16 px-36 h-full flex-grow flex flex-col justify-center animate__animated animate__fadeIn'>
       <div className='pb-8'>
         <div className='text-5xl pb-1'><b className='text-slate-400'>E</b>ncrypted <b className='text-slate-400'>S</b>tudent <b className='text-slate-400'>D</b>ata <b className='text-slate-400'>A</b>nalytics <b className='text-slate-400'>P</b>latform</div>
         <div className='font-light text-sm'>(Using Homomorphic Encryption)</div>
