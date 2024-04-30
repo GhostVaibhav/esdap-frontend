cefb38ddea38162e620c5f790ffe2a5c101343d4
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2024-04-30 23:10:13 +0530

    refactor(navigation): changed the scope of details page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,6 +10,6 @@ export const nav = [
      { path:     "/about",    name: "About",       element: <AboutCard />,      isMenu: true,     isPrivate: false  },
      { path:     "/login",    name: "Login",       element: <LoginCard />,      isMenu: false,    isPrivate: false  },
      { path:     "/faq",  name: "FAQ",     element: <FAQCardEx />,    isMenu: true,     isPrivate: false  },
-     { path:     "/result",  name: "Result",     element: <ResultCard />,    isMenu: true,     isPrivate: true  },
+     { path:     "/result",  name: "Result",     element: <ResultCard />,    isMenu: false,     isPrivate: true  },
      { path:     "/details",  name: "Details",     element: <StudentDetailsForm />,    isMenu: true,     isPrivate: true  }
 ]
